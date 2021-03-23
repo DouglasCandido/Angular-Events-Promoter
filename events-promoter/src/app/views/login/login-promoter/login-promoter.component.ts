ee6503dde6dece561a5480686e0feaf869af1065
DouglasCandido <douglasmateus1@hotmail.com>
2021-03-23 16:36:32 -0300

    Last updates.

--- events-promoter/src/app/views/login/login-promoter/login-promoter.component.ts
+++ events-promoter/src/app/views/login/login-promoter/login-promoter.component.ts
@@ -1,4 +1,7 @@
+import { PromoterService } from './../../../controllers/promoter.service';
+import { Promoter } from './../../../models/promoter.model';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-promoter',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPromoterComponent implements OnInit {
 
-  constructor() { }
+  promoter: Promoter = {
+
+    username: "",
+    password_promoter: "",
+
+  }
+
+  imageLink = "assets/img/background.png";
+
+  constructor(private promoterService: PromoterService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  loginPromoter(): void {
+
+    this.promoterService.login(this.promoter.username, this.promoter.password_promoter).subscribe(() => {
+    this.promoterService.showMessage("Você entrou no sistema com sucesso!");
+    this.router.navigate(["/home_promoter"]);
+    });
+
+  }
+
+  cancel(): void {
+
+    this.router.navigate(["/login"]);
+    
+  }
+
 }
+
+

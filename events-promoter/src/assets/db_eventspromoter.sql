c15a9a3c90b9bcbd25c7e66120d6de524b34353e
DouglasCandido <douglasmateus1@hotmail.com>
2021-04-15 09:22:56 -0300

    Atualizações no esquema do banco de dados.

--- events-promoter/src/assets/db_eventspromoter.sql
+++ events-promoter/src/assets/db_eventspromoter.sql
@@ -6,47 +6,43 @@ use db_eventspromoter;
 
 # ALTER DATABASE db_events_promoter CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create table if not exists uf
-(
+# Tabela utilizada para armazenar os estados cadastrados no sistema
+create table if not exists uf(
 
-    id integer not null unique auto_increment,
-    sigla char(2) not null unique,
-    nome varchar(20) not null unique,
-    primary key(sigla)
-
+    # codigo int not null auto_increment,
+    sigla varchar(2) not null,
+    nome varchar(25) not null,
+    primary key(sigla) 
+    
 );
 
-select * from uf;
-
-drop table uf;
-
-insert into uf(sigla, nome) values('AC','Acre');
-insert into uf(sigla, nome) values('AL','Alagoas');
-insert into uf(sigla, nome) values('AP','Amapá');
-insert into uf(sigla, nome) values('AM','Amazonas');
-insert into uf(sigla, nome) values('BA','Bahia');
-insert into uf(sigla, nome) values('CE','Ceará');
-insert into uf(sigla, nome) values('DF','Distrito Federal');
-insert into uf(sigla, nome) values('ES','Espírito Santo');
-insert into uf(sigla, nome) values('GO','Goiás');
-insert into uf(sigla, nome) values('MA','Maranhão');
-insert into uf(sigla, nome) values('MT','Mato Grosso');
-insert into uf(sigla, nome) values('MS','Mato Grosso do Sul');
-insert into uf(sigla, nome) values('MG','Minas Gerais');
-insert into uf(sigla, nome) values('PA','Pará');
-insert into uf(sigla, nome) values('PB','Paraíba');
-insert into uf(sigla, nome) values('PR','Paraná');
-insert into uf(sigla, nome) values('PE','Pernambuco');
-insert into uf(sigla, nome) values('PI','Piauí');
-insert into uf(sigla, nome) values('RJ','Rio de Janeiro');
-insert into uf(sigla, nome) values('RN','Rio Grande do Norte');
-insert into uf(sigla, nome) values('RS','Rio Grande do Sul');
-insert into uf(sigla, nome) values('RO','Rondônia');
-insert into uf(sigla, nome) values('RR','Roraima');
-insert into uf(sigla, nome) values('SC','Santa Catarina');
-insert into uf(sigla, nome) values('SP','São Paulo');
-insert into uf(sigla, nome) values('SE','Sergipe');
-insert into uf(sigla, nome) values('TO','Tocantins');
+insert into uf(sigla, nome) values("AC","Acre");
+insert into uf(sigla, nome) values("AL","Alagoas");
+insert into uf(sigla, nome) values("AP","Amapá");
+insert into uf(sigla, nome) values("AM","Amazonas");
+insert into uf(sigla, nome) values("BA","Bahia");
+insert into uf(sigla, nome) values("CE","Ceará");
+insert into uf(sigla, nome) values("DF","Distrito Federal");
+insert into uf(sigla, nome) values("ES","Espírito Santo");
+insert into uf(sigla, nome) values("GO","Goiás");
+insert into uf(sigla, nome) values("MA","Maranhão");
+insert into uf(sigla, nome) values("MT","Mato Grosso");
+insert into uf(sigla, nome) values("MS","Mato Grosso do Sul");
+insert into uf(sigla, nome) values("MG","Minas Gerais");
+insert into uf(sigla, nome) values("PA","Pará");
+insert into uf(sigla, nome) values("PB","Paraíba");
+insert into uf(sigla, nome) values("PR","Paraná");
+insert into uf(sigla, nome) values("PE","Pernambuco");
+insert into uf(sigla, nome) values("PI","Piauí");
+insert into uf(sigla, nome) values("RJ","Rio de Janeiro");
+insert into uf(sigla, nome) values("RN","Rio Grande do Norte");
+insert into uf(sigla, nome) values("RS","Rio Grande do Sul");
+insert into uf(sigla, nome) values("RO","Rondônia");
+insert into uf(sigla, nome) values("RR","Roraima");
+insert into uf(sigla, nome) values("SC","Santa Catarina");
+insert into uf(sigla, nome) values("SP","São Paulo");
+insert into uf(sigla, nome) values("SE","Sergipe");
+insert into uf(sigla, nome) values("TO","Tocantins");
 
 create table if not exists enthusiast
 (
@@ -58,15 +54,15 @@ create table if not exists enthusiast
     birthDate date not null,
     username varchar(16) not null unique,
     password_enthusiast varchar(16) not null,
-    contactEmail varchar(20) not null unique,
-    contactPhone varchar(13) not null unique,
+    contactEmail varchar(30) not null unique,
+    contactPhone varchar(11) not null unique,
     state char(2) not null,
     city varchar(56) not null,
     district varchar(56) not null,
     street varchar(56) not null,
     number_place integer not null,
     zipCode varchar(9) not null,
-    registrationDate date not null,
+    registrationDate datetime not null,
     primary key(cpf),
     foreign key(state) references uf(sigla) 
 
@@ -95,7 +91,7 @@ create table if not exists promoter
     number_place integer not null,
     zipCode varchar(9) not null,
     site varchar(30) unique,
-    registrationDate date not null,
+    registrationDate datetime not null,
     primary key(cnpj),
     foreign key(state) references uf(sigla) 
 
@@ -110,7 +106,7 @@ create table if not exists event
     id integer not null unique auto_increment,
     cnpj_promoter varchar(18) not null,
     name_event varchar(50) not null,
-    description_event varchar(50) not null,
+    description_event varchar(100) not null,
     theme varchar(100) not null,
     state char(2) not null,
     city varchar(56) not null,
@@ -121,9 +117,10 @@ create table if not exists event
     latitude numeric(10, 6) not null,
     longitude numeric(10, 6) not null,
     site varchar(30) unique,
-    eventDate date not null,
+    eventDate datetime not null,
     primary key(id),
-    foreign key (cnpj_promoter) references promoter(cnpj)
+    foreign key (cnpj_promoter) references promoter(cnpj),
+    foreign key (state) references uf(sigla)
 );
 
 select * from event;
@@ -136,7 +133,7 @@ create table if not exists comment
     comment_about_the_event varchar(100) not null,
     cpf_enthusiast varchar(14) not null,
     id_event integer not null,
-    commentRegistrationDate date not null,
+    commentRegistrationDate datetime not null,
     primary key(id),
     foreign key(cpf_enthusiast) references enthusiast(cpf),
     foreign key(id_event) references event(id)
@@ -149,7 +146,6 @@ drop table comment;
 create table if not exists measureOfInterestInTheEvent
 (
     id integer not null unique auto_increment,
-    cpf_enthusiast varchar(14) not null,
     id_event integer not null,
     numberOfEnthusiastsWhoDeclaredInterestInAttending integer default 0,
     likes integer default 0,
@@ -163,4 +159,20 @@ select * from measureOfInterestInTheEvent;
 
 drop table measureOfInterestInTheEvent;
 
+# Tabela utilizada para armazenar as inscrições dos entusiastas em eventos
+create table if not exists subscription(
+
+	id integer not null unique auto_increment,
+    id_event integer not null,
+    cpf_enthusiast varchar(14) not null,
+    subscriptionRegistrationDate datetime not null,
+	primary key(id),
+    foreign key(cpf_enthusiast) references enthusiast(cpf),
+    foreign key(id_event) references event(id)
+    
+);
+
+select * from subscription;
+
+drop table subscription;
 

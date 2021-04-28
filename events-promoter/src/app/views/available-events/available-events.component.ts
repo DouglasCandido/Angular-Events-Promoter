236dd50653c63522c03f5426e6cafbf50c7effd9
DouglasCandido <douglasmateus1@hotmail.com>
2021-04-28 10:04:52 -0300

    Pesquisa por filtros implementada.

--- events-promoter/src/app/views/available-events/available-events.component.ts
+++ events-promoter/src/app/views/available-events/available-events.component.ts
@@ -11,6 +11,14 @@ export class AvailableEventsComponent implements OnInit {
 
   events: Event[];
 
+  event: Event = {
+    name_event: '',
+    theme: '',
+    state: '',
+    city: '',
+    zipCode: ''
+  }
+
   displayedColumns = ["name_event", "description_event", "theme", "state", "city", "district", "street", "number_place", "zipCode", "latitude", "longitude", "site", "eventDate", "createdAt", "action"];
 
   constructor(private eventService: EventService) {
@@ -24,6 +32,66 @@ export class AvailableEventsComponent implements OnInit {
     this.eventService.findAll().subscribe(
       events => {this.events = events}
     );
+
+  }
+
+  searchEventByName(): void {
+    
+    this.eventService.findAllByName(this.event.name_event).subscribe((eventos_encontrados) => {
+
+      this.eventService.showMessage("Eventos encontrados com sucesso!");
+
+      this.events = eventos_encontrados;
+
+    });
+
+  }
+
+  searchEventByTheme(): void {
+    
+    this.eventService.findAllByTheme(this.event.theme).subscribe((eventos_encontrados) => {
+
+      this.eventService.showMessage("Eventos encontrados com sucesso!");
+
+      this.events = eventos_encontrados;
+
+    });
+
+  }
+
+  searchEventByCity(): void {
+    
+    this.eventService.findAllByCity(this.event.city).subscribe((eventos_encontrados) => {
+
+      this.eventService.showMessage("Eventos encontrados com sucesso!");
+
+      this.events = eventos_encontrados;
+
+    });
+
+  }
+
+  searchEventByState(): void {
+    
+    this.eventService.findAllByState(this.event.state).subscribe((eventos_encontrados) => {
+
+      this.eventService.showMessage("Eventos encontrados com sucesso!");
+
+      this.events = eventos_encontrados;
+
+    });
+
+  }
+
+  searchEventByZipCode(): void {
+    
+    this.eventService.findAllByZipCode(this.event.zipCode).subscribe((eventos_encontrados) => {
+
+      this.eventService.showMessage("Eventos encontrados com sucesso!");
+
+      this.events = eventos_encontrados;
+
+    });
 
   }
 

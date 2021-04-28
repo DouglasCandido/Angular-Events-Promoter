39f63b9cf423437d542420d8ef4e6aa251e71d34
DouglasCandido <douglasmateus1@hotmail.com>
2021-04-28 12:44:29 -0300

    O core essencial do projeto foi implementado e acredito que o objetivo tenha sido alcançado. Para implementações futuras, eu gostaria de subir o projeto pra produção, implementar a visualização da localização do evento através de uma API de mapa, validar os campos dos formulários, adicionar a funcionalidade de vendas de ingressos para os eventos e a funcionalidade de feedback com comentários e likes / dislikes sobre os eventos.

--- events-promoter/src/app/components/subscription-delete/subscription-delete.component.ts
+++ events-promoter/src/app/components/subscription-delete/subscription-delete.component.ts
@@ -1,4 +1,8 @@
+import { SubscriptionService } from './../../controllers/subscription.service';
+import { Subscription } from './../../models/subscription.model';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { DatePipe, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-subscription-delete',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionDeleteComponent implements OnInit {
 
-  constructor() { }
+  subscription: Subscription = {
+    id: null,
+    id_event: null,
+    cpf_enthusiast: "",
+    createdAt: null
+  }
+
+  data_full: string;
+
+  data_formatada: string;
+
+  constructor(private subscriptionService: SubscriptionService, private router: Router, private route: ActivatedRoute) { 
+
+
+
+  }
 
   ngOnInit(): void {
+
+    const id = +this.route.snapshot.paramMap.get("id");
+
+    this.subscriptionService.findOne(id).subscribe(subscription => {
+
+      this.subscription = subscription;
+
+      this.data_full = formatDate(this.subscription.createdAt, 'full', 'pt-BR', '-0300');
+
+      this.data_formatada = new Date(formatDate(this.subscription.createdAt, 'medium', 'en-US', '-0600')).toISOString().slice(0,16);
+
+    }
+
+    )
+
+  }
+
+  deleteSubscription(): void {
+
+    this.subscriptionService.deleteOne(this.subscription.id).subscribe(() => {
+      this.subscriptionService.showMessage("Inscrição cancelada com sucesso!");
+      this.router.navigate(["/home_enthusiast/events_i_am_interested_in_atending"]);
+    });
+
+  }
+
+  cancel(): void {
+
+    this.router.navigate(["/home_enthusiast/events_i_am_interested_in_atending"]);
+
   }
 
 }
+
--- events-promoter/src/app/components/subscription-details/subscription-details.component.ts
+++ events-promoter/src/app/components/subscription-details/subscription-details.component.ts
@@ -1,3 +1,7 @@
+import { DatePipe, formatDate } from '@angular/common';
+import { Event } from '../../models/event.model';
+import { EventService } from '../../controllers/event.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionDetailsComponent implements OnInit {
 
-  constructor() { }
+  event: Event = {
+
+    name_event: "",
+    description_event: "",
+    theme: "",
+    state: "",
+    city: "",
+    district: "",
+    street: "",
+    number_place: null,
+    zipCode: "",
+    latitude: null,
+    longitude: null,
+    site: "",
+    eventDate: null
+
+  }
+
+  data_full: string;
+
+  data_formatada: string;
+
+  constructor(private eventService: EventService, private router: Router, private route: ActivatedRoute) { 
+
+
+
+  }
 
   ngOnInit(): void {
+
+    const id = +this.route.snapshot.paramMap.get("id_event");
+
+    this.eventService.findOne(id).subscribe(event => {
+
+    this.event = event;
+
+    this.data_full = formatDate(this.event.eventDate, 'full', 'pt-BR', '-0300');
+
+    this.data_formatada = new Date(formatDate(this.event.eventDate, 'medium', 'en-US', '-0600')).toISOString().slice(0,16);
+      
+  });
+
+  }
+
+  back(): void {
+
+    this.router.navigate(["/home_enthusiast/events_i_am_interested_in_atending"]);
+
   }
 
 }
+
